4be16e87bb11fa488357591d8dfa93d84dafb44a
Rafsan Bhuiyan <48545950+rafsanbhuiyan@users.noreply.github.com>
2023-05-28 14:33:31 -0600

    added new functions

---
+++ src/functions/transformation_functions.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from functions.life_policy_fucntions import assign_policyholder_id
+
+
+# Transform Raw Data
+def process_raw_data(df, map_dict, col_order):
+    # Rename column in the DataFrame
+    df.rename(columns=map_dict, inplace=True)
+    # Replace NaN with None
+    df = df.where(pd.notnull(df), None)
+    # Create numeric Unique Ids based on Primary Policy_Holder Names using assign_policy_holder_function
+    df = assign_policyholder_id(df=df,
+                                username_col_str="primary_name",
+                                id_col_str="policy_holder_id")
+    # Reindex the DF
+    df = df.reindex(columns=col_order)
+    print("Raw Dataframe Ready")
+
+    return df
+
+
+# Select Columns for policy data
+def select_column_policy_data(df, select_cols):
+    #Select Columns
+    df = df[select_cols]
+
+    return df
+
+
+# Transform Policy Holder Dataframe
+def process_policy_holder_data(df, map_dict, select_col):
+    # Rename
+    df = df.rename(columns=map_dict)
+    # Select Columns
+    df = df[select_col]
+
+    return df
--- src/life_policy.py
+++ src/life_policy.py
@@ -1,7 +1,13 @@
 from core.utility import get_resouce, load_yaml
-from functions.life_policy_fucntions import assign_policyholder_id, dedup_policy
+from functions.life_policy_fucntions import dedup_policy
+from functions.transformation_functions import process_raw_data, select_column_policy_data, process_policy_holder_data
 import service.mysql as mysql
 import pandas as pd
+
+# CONSTANT VARIABLE
+POLICY_DATA_COLUMN_SELECTION = ['number', 'policy_holder_id', 'data_provider_code', 'data_provider_description',
+                                'data_provider_priority', 'effective_date', 'issue_date', 'maturity_date',
+                                'origination_death_benefit', 'carrier_name']
 
 # Setting Environment Variables
 # Parameter File Name
@@ -18,67 +24,52 @@ db = mysql.MySQLConnector(params["mysql_creds"]["host"],
 
 # Stage Data
 
-# Convert Data to DataFrame
+# Ingest Input CSV File and Convert to DataFrame
 life_policy_raw_df = pd.read_csv("life_policy_data.csv")
 
 # Transform Data
 
 # Step 1 Transform and Prepare Raw Data
 # Rename Columns
-# Column Mapping
 str_map_dict = {"name_1": "primary_name", "gender_1": "primary_gender", "birth_date_1": "primary_birth_date",
                 "name_2": "secondary_name", "gender_2": "secondary_gender", "birth_date_2": "secondary_birth_date"}
-
-# Rename column in the DataFrame
-life_policy_raw_df.rename(columns=str_map_dict, inplace=True)
-
-# Replace NaN with None
-life_policy_raw_df = life_policy_raw_df.where(pd.notnull(life_policy_raw_df), None)
-
-# Create numeric Unique Ids based on Primary Policy_Holder Names using assign_policy_holder_function
-life_policy_raw_df = assign_policyholder_id(df=life_policy_raw_df,
-                                            username_col_str="primary_name",
-                                            id_col_str="policy_holder_id")
-
 # Rearrange Columns
 column_order = ["number", "policy_holder_id", "data_provider_code", "data_provider_description",
                 "data_provider_priority", "effective_date", "issue_date",
                 "maturity_date", "origination_death_benefit", "carrier_name",
                 "primary_name", "primary_gender", "primary_birth_date",
                 "secondary_name", "secondary_gender", "secondary_birth_date"]
+# Transform raw data using process_raw_data() function
+life_policy_raw_df = process_raw_data(df=life_policy_raw_df, map_dict=str_map_dict,
+                                      col_order=column_order)
+# Step 1 Complete
 
-# Reindex the DF
-life_policy_raw_df = life_policy_raw_df.reindex(columns=column_order)
-# Raw data ready
-
-# Step 2 Transform: Deduplicate by Name and Age, keeping the one provided by the highest priority provider
-life_policy_normalized_df = dedup_policy(df=life_policy_raw_df, name="primary_name"
-                                         , dob="primary_birth_date", priority="data_provider_priority")
-
+# Step 2 Process Normalized Data
+# Deduplicate by Name and Age, keeping the one provided by the highest priority provider
+life_policy_normalized_df = dedup_policy(df=life_policy_raw_df, name="primary_name",
+                                         dob="primary_birth_date", priority="data_provider_priority")
 # Reset Index
 life_policy_normalized_df.reset_index(drop=True, inplace=True)
 
-# Prepare life_policy_normalized dataset
-select_cols = ['number', 'policy_holder_id', 'data_provider_code','data_provider_description',
-               'data_provider_priority', 'effective_date', 'issue_date', 'maturity_date',
-               'origination_death_benefit', 'carrier_name']
-
-# Step 2: Transformation before Insert to policy_normalized table in life_policy_db schema
 # Assign subset from life_policy_normalized_df to policy_normalized_df
-policy_normalized_df = life_policy_normalized_df[select_cols]
+# Assign subset from life_policy_normalized_df to policy_normalized_df
+policy_normalized_df = select_column_policy_data(df=life_policy_normalized_df,
+                                                 select_cols=POLICY_DATA_COLUMN_SELECTION)
+# Step 2 Complete
 
-# Step 3: Get subset of columns from life_policy_normalized_df and assign to policy_holders_df
+# Step 3 Process Policy_Holder Data
+# Transform: Get subset of columns from life_policy_normalized_df and assign to policy_holders_df
 # Transformation before Insert to policy_holders table in life_policy_db schema
 
-# Rename columns for policy_holder dataset
+# String Mapping for renaming columns
 str_map_dict = {'policy_holder_id': 'id', 'number': 'policy_number'}
-policy_holder_df = life_policy_normalized_df.rename(columns=str_map_dict)
-
 # Select Correct Columns for policy_holder dataset
-select_cols = ['id', 'policy_number', 'primary_name', 'primary_gender', 'primary_birth_date',
-               'secondary_name', 'secondary_gender', 'secondary_birth_date']
+select_columns = ['id', 'policy_number', 'primary_name', 'primary_gender', 'primary_birth_date',
+                  'secondary_name', 'secondary_gender', 'secondary_birth_date']
 
-policy_holders_df = policy_holder_df[select_cols]
+# Process Policy_Holder Data
+policy_holders_df = process_policy_holder_data(df=life_policy_normalized_df, map_dict=str_map_dict,
+                                               select_col=select_columns)
 
 # Step 4 Transform: Append all remaining policies from the raw data, which are not present
 # in the 'policy_normalized_df', to the 'policy_surplus_records_df'
@@ -97,23 +88,17 @@ policy_surplus_records_df = life_policy_raw_df[select_cols]
 # Convert 'policy_normalized_df['number']' column to list
 exclude_policy_ids = list(policy_normalized_df['number'])
 
+# Filter policy_surplus_records_df
 policy_surplus_records_df = \
     policy_surplus_records_df[~policy_surplus_records_df['number'].isin(exclude_policy_ids)]
 
-#TODO: TESTING
-#life_policy_normalized_df.to_csv("normalized_test1.csv", index=False)
-
+# Reset Index
+policy_surplus_records_df.reset_index(drop=True, inplace=True)
 
 
 # TODO: FOR LATER
 # Insert data from life_policy_raw_df to "policy_all_data" table in life_policy_db schema
 #db.insert_data("policy_all_data", life_policy_raw_df)
-
-
-# 2 Normalize and dedup raw data
-
-
-
 
 #Close Connection
 db.close_connection()
