d919e3c067e3c05a6c4de5481ef20bf867e60071
Rafsan Bhuiyan <48545950+rafsanbhuiyan@users.noreply.github.com>
2023-05-29 21:45:41 -0600

    updated flask app

--- src/flask_app.py
+++ src/flask_app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify
+from flask_restx import Api, Resource
+
 import service.MySQLConnector as mysql
 from core.utility import get_resouce, load_yaml
 
@@ -6,7 +8,8 @@ from core.utility import get_resouce, load_yaml
 SELECT_DB = """USE life_policy_db;"""
 
 app = Flask(__name__)
-
+api = Api(app, version='3.0.0', title='Flask API', description='API endpoints for life policy management',
+          default='Life Policy Project')
 
 # Setting Environment Variables
 param_file_name = "params.yml"
@@ -25,32 +28,55 @@ db.connect_to_db(params["mysql_creds"]["database"])
 # SELECT DB
 db.run_query(SELECT_DB)
 
-g = db.get_person_policies("Alfred Young")
-print(g)
 
-@app.route('/')
-def home():
-    return "Welcome to the Flask API!"
+@api.route('/')
+class Home(Resource):
+    def get(self):
+        """
+        Welcome message
+        """
+        return "Welcome to the Flask API!"
 
-@app.route('/policy-info/<string:policy_number>', methods=['GET'])
-def policy_info(policy_number):
-    policy_info = db.get_policy_info(policy_number)
-    return jsonify(policy_info)
 
-@app.route('/carrier-policy-count/<string:carrier_name>', methods=['GET'])
-def carrier_policy_count(carrier_name):
-    count = db.get_carrier_policy_count(carrier_name)
-    return jsonify({'count': count})
+@api.route('/policy-info/<string:policy_number>')
+class PolicyInfo(Resource):
+    def get(self, policy_number):
+        """
+        Returns policy information for a specific policy number.
+        """
+        policy_info = db.get_policy_info(policy_number)
+        return jsonify(policy_info)
 
-@app.route('/person-policies/<string:person_name>', methods=['GET'])
-def person_policies(person_name):
-    policies = db.get_person_policies(person_name)
-    return jsonify(policies)
 
-@app.route('/data-provider-policies/<string:data_provider_code>', methods=['GET'])
-def data_provider_policies(data_provider_code):
-    count = db.get_data_provider_policy_count(data_provider_code)
-    return jsonify({'count': count})
+@api.route('/carrier-policy-count/<string:carrier_name>')
+class CarrierPolicyCount(Resource):
+    def get(self, carrier_name):
+        """
+        Returns the count of policies for a specific carrier.
+        """
+        count = db.get_carrier_policy_count(carrier_name)
+        return jsonify({'count': count})
+
+
+@api.route('/person-policies/<string:person_name>')
+class PersonPolicies(Resource):
+    def get(self, person_name):
+        """
+        Returns the policies associated with a specific person.
+        """
+        policies = db.get_person_policies(person_name)
+        return jsonify(policies)
+
+
+@api.route('/data-provider-policies/<string:data_provider_code>')
+class DataProviderPolicies(Resource):
+    def get(self, data_provider_code):
+        """
+        Returns the count of policies from a specific data provider.
+        """
+        count = db.get_data_provider_policy_count(data_provider_code)
+        return jsonify({'count': count})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
